8fa8378501b5b745e48b0197c8bde914a77e8da8
israel <cuquitoups@gmali.com>
2022-01-13 13:25:19 -0600

    creacion de rutas globales de la aplicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TarjetasComponent} from "./tarjetas/tarjetas.component";
+import {DetalleComponent} from "./detalle/detalle.component";
+import {HistorialComponent} from "./historial/historial.component";
+import {ReporteComponent} from "./reporte/reporte.component";
+import {DiarioComponent} from "./diario/diario.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"tarjetas", component:TarjetasComponent },
+  { path:"detalle",component:DetalleComponent },
+  { path:"historial", component:HistorialComponent},
+  { path:"diario", component:DiarioComponent },
+  { path:"reporte", component:ReporteComponent},
+  { path: '', redirectTo: 'tarjetas', pathMatch: 'full' },
+  { path: '**', redirectTo: 'tarjetas', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

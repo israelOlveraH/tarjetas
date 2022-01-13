1509c1729aab7548aead987268089ede9be020b1
israel <cuquitoups@gmali.com>
2022-01-13 13:22:52 -0600

    declaracion de componentes tarjetas,detalle,histrial,reporte,diario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarjetasComponent } from './tarjetas/tarjetas.component';
+import { DetalleComponent } from './detalle/detalle.component';
+import { HistorialComponent } from './historial/historial.component';
+import { ReporteComponent } from './reporte/reporte.component';
+import { DiarioComponent } from './diario/diario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetasComponent,
+    DetalleComponent,
+    HistorialComponent,
+    ReporteComponent,
+    DiarioComponent
   ],
   imports: [
     BrowserModule,

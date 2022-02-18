57575b2935feb0fc7e9c409f8e07ee98e592ae11
israel <cuquitoups@gmali.com>
2022-02-17 21:33:36 -0600

    correccion lista de registros

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  // Declaracion de variables
+  public formulario: FormGroup;
 
+  constructor(private firestore: AngularFirestore) {
+    // Inicializacion de variables (Asignarles un valor inicial)
+    this.formulario = new FormGroup({
+      nombre: new FormControl(""),
+      apat: new FormControl(""),
+      amat: new FormControl(""),
+      dir: new FormControl(""),
+      ref: new FormControl(""),
+      art: new FormControl(""),
+      pre: new FormControl(""),
+      cli: new FormControl(""),
+    });
+  }
+
+  // Metodos
   ngOnInit(): void {
+  }
+
+  public registroFormulario(): void {
+    console.log(this.formulario.value)
+    this.firestore.collection("registroTarjetas").add(this.formulario.value)
   }
 
 }

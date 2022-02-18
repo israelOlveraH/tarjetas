a2046eb1d0a88b5372724eb00aeb9ea5df281f2f
israel <cuquitoups@gmali.com>
2022-02-17 21:35:18 -0600

    coneccion con firestore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: 'tar1-724de',
+    appId: '1:342318702998:web:5919e9e4c1e91c7763fdb7',
+    storageBucket: 'tar1-724de.appspot.com',
+    locationId: 'northamerica-northeast1',
+    apiKey: 'AIzaSyCa1QnEKgWlTdF08RJR0g7xyw864k28RBc',
+    authDomain: 'tar1-724de.firebaseapp.com',
+    messagingSenderId: '342318702998',
+    measurementId: 'G-2JVQRKZWF7',
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'tar1-724de',
+    appId: '1:342318702998:web:5919e9e4c1e91c7763fdb7',
+    storageBucket: 'tar1-724de.appspot.com',
+    locationId: 'northamerica-northeast1',
+    apiKey: 'AIzaSyCa1QnEKgWlTdF08RJR0g7xyw864k28RBc',
+    authDomain: 'tar1-724de.firebaseapp.com',
+    messagingSenderId: '342318702998',
+    measurementId: 'G-2JVQRKZWF7',
+  }
 };
 
 /*
